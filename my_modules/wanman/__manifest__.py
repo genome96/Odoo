4678b11542220da5554aa4f913149614e12d63de
Ian Karanja <optimusglitch@gmail.com>
2024-06-18 11:36:11 +0300

    Added Files

---
+++ my_modules/wanman/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Wan Manager',
+    'version': '1.0.0',
+    'summary': 'Internet Subscriber Management',
+    'sequence': -10,
+    'description': """Subscriber Management System
+===========================
+The Subscriber Management System in Odoo offers a user-friendly way to manage your internet service subscribers efficiently, even if you don't have a background in customer management. This system enables you to monitor your subscribers and their internet packages seamlessly.
+
+This module is ideal if you collaborate with an external service provider to manage your subscriptions but still want to keep track of your subscribers' activities and payments. It also provides an easy method of registering new subscribers and their payments, without the need to handle complex customer databases. 
+""",
+    'depends': ['base',],
+    'Author': "Ian Karanja",
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
